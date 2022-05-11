b124169bded3198a77570181947817212ae4b993
Siddharth-Manur <siddharthmanur2003@gmail.com>
2022-05-11 21:59:55 +0530

    edited

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -3,3 +3,4 @@ text = "X-DSPAM-Confidence:    0.8475"
 pos = text.find(':')#index of ':'
 prnt = text[pos+1:]#store the elements of the string after ':'
 value = float(prnt)#converting the string into floating point number
+print(value)
