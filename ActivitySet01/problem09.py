b341d5784084c5315d69bcceb3b9f8749e1c84f9
Siddharth-Manur <siddharthmanur2003@gmail.com>
2022-05-16 10:19:21 +0530

    done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,11 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fh = open(filename)
+c=fh.readlines()
+words=list()
+for i in c:
+    for j in i.split():
+        if j not in words:
+            words.append(j)
+print(sorted(words))
