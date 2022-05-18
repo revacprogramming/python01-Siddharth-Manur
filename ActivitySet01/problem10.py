b0859bd48c88fe09bdc62f2a5a7406f5bdad9bd0
Siddharth-Manur <siddharthmanur2003@gmail.com>
2022-05-18 21:54:45 +0530

    done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,21 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+filehandle = open(filename)
+dt = dict()
+
+for line in filehandle:
+    if len(line) < 3 or not line.startswith("From "): continue
+    words = line.strip().split()
+    word = words[1]
+    dt[word] = dt.get(word,0) + 1
+
+sender = None
+no_of_messages = None
+
+for name,count in dt.items():
+    if no_of_messages == None or count > no_of_messages :
+        sender = name
+        no_of_messages = count
+
+print(sender,no_of_messages)
