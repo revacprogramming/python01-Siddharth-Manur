cefba0ac6bf66666a9848ff861a4114d1f33e7bc
Siddharth-Manur <siddharthmanur2003@gmail.com>
2022-05-23 21:13:57 +0530

    done

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,14 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+handle = open(filename)
+d=dict()
+for line in handle:
+	if len(line) < 3 or not line.startswith("From "): continue
+	words = line.strip().split()
+	word = words[5]
+	time = word.split(':')
+	hours = time[0]
+	d[hours] = d.get(hours,0) + 1
+for key, val in sorted(d.items()):
+    print(key, val)
